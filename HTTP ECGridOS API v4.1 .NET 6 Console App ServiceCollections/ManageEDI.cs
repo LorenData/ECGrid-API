067b70ddef9045b89471d5ab42223552c23213c6
Greg Kolinski <32110102+GregKolinski@users.noreply.github.com>
2022-09-28 19:59:07 -0500

    Add files via upload

---
+++ HTTP ECGridOS API v4.1 .NET 6 Console App ServiceCollections/ManageEDI.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_ECGridOS_API_v4._1_.NET_6_Console_App_ServiceCollections;
+
+internal interface IManageEDI
+{
+    Task<string> CheckECGridOSIsRunning();
+
+    Task<string> CheckECGridOSSession(string APIKey);
+
+    Task<int> DownloadFiles(string APIKey);
+
+    Task<int> UploadloadFile(string APIKey);
+}
+
+internal class ManageEDI : IManageEDI
+{
+    #region Class Variables
+
+    private readonly IECGridOSClient _ECGridOSClient;
+
+    #endregion
+
+    #region Constructor
+
+    public ManageEDI(IECGridOSClient ECGridOSClient)
+    {
+        _ECGridOSClient = ECGridOSClient;
+    }
+
+    #endregion
+
+    public async Task<string> CheckECGridOSIsRunning()
+    {
+        // Check if the EGridOS API is running
+        string ecgridos_version = await _ECGridOSClient.Version();
+        if (!string.IsNullOrEmpty(ecgridos_version))
+        {
+            return $"ECGridOS API is Running: {ecgridos_version}";
+        }
+
+        return String.Empty;
+    }
+
+    public async Task<string> CheckECGridOSSession(string APIKey)
+    {
+        // Check that My API Key can access a persistant session.
+        SessionInfo sessionInfo = await _ECGridOSClient.WhoAmI(APIKey);
+        if (!string.IsNullOrEmpty(sessionInfo.SessionID))
+        {
+            Console.WriteLine($"ECGridOS API has a persistant Session ID: {sessionInfo.SessionID}");
+        }
+
+        return String.Empty;
+    }
+
+    public async Task<int> DownloadFiles(string APIKey)
+    {
+        int downloadedFiles = 0;
+
+        // Check if there are any Inbound Files to Download
+        ParcelIDInfoCollection parcelPending = await _ECGridOSClient.ParcelInBox(APIKey);
+
+        // Check for Pending Documents to Download
+        if (parcelPending != null && parcelPending.TotalRecords > 0)
+        {
+            // Do For Each File
+            foreach (ParcelIDInfo parcel2Download in parcelPending.ParcelIDInfoList)
+            {
+                long parcelID = parcel2Download.ParcelID;
+
+                // Download the File
+                FileInfo file2Save = await _ECGridOSClient.ParcelDownload(APIKey, parcelID);
+                if (file2Save != null)
+                {
+                    // Save Base64 String
+                    string FileAsBase64 = file2Save.ContentBase64String;
+
+                    // Make the Filename
+                    string fileName = Path.Combine("c:/temp/download", file2Save.FileName);
+
+                    // Write to File however you want
+                    File.WriteAllBytes(fileName, file2Save.Content);
+
+                    // File was written
+                    if (File.Exists(fileName))
+                    {
+                        // Confirm that the File was Download and remove it from the Pending List on ECGridOS
+                        if (await _ECGridOSClient.ParcelDownloadConfirm(APIKey, parcelID))
+                        {
+                            Console.WriteLine($"ECGridOS API File Downloaded and written to disk: {fileName}");
+                            downloadedFiles++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return downloadedFiles;
+    }
+
+    public async Task<int> UploadloadFile(string APIKey)
+    {
+        // Upload File to ECGridOS API
+        string file2Upload = Path.Combine("c:/temp/upload", "Upload.edi");
+        string FilenameNoPath = Path.GetFileName(file2Upload);
+        byte[] contents2Upload = File.ReadAllBytes(file2Upload);
+
+        // Upload A File
+        int returnedParcelID = await _ECGridOSClient.ParcelUpload(APIKey, FilenameNoPath, contents2Upload.Length, contents2Upload);
+
+        // Check the File Was Uploaded
+        if (returnedParcelID > 0)
+        {
+            Console.WriteLine($"ECGridOS API File Uploaded: {FilenameNoPath}");
+            return returnedParcelID;
+        }
+
+        return -1;
+    }
+}
---
+++ HTTP ECGridOS API v4.1 .NET 6 Console App ServiceCollections/Program.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * ECGridOS API Console App Example for .NET 6 using Service Collections
+ * Copyright: Loren Data Corp.
+ * Last Updated: 09/28/2022
+ * Author: Greg Kolinski
+ * Description: Example Calls to provide users with a basic example of using the ECGridOS API Service with .NET 6 Projects
+ * Examples are not production ready and you will need to impliment your own best practices.
+ * Provided as Example only
+ * 
+ * Only Covers Connecting Via HTTP in a ServiceCollection
+ * 
+ * Example uses Nuget Packages
+ * Microsoft.Extensions.DependencyInjection
+ * Microsoft.Extensions.Http
+ * 
+ */
+
+using HTTP_ECGridOS_API_v4._1_.NET_6_Console_App_ServiceCollections;
+
+using Microsoft.Extensions.DependencyInjection;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Use the SecretManager or keep/reference the APIKey somewhere secure, we do not recommend having the key in your source code. 
+string MyAPIKey = "FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF";
+
+var services = new ServiceCollection();
+services.AddScoped<IManageEDI, ManageEDI>();
+services.AddHttpClient<IECGridOSClient, ECGridOSClient>(client =>
+{
+    client.BaseAddress = new Uri("https://os.ecgrid.io/v4.1/prod/ECGridOS.asmx/");
+    client.Timeout = TimeSpan.FromMinutes(10);
+}).SetHandlerLifetime(TimeSpan.FromMinutes(720)); // Reuse connection pool for 12 hour before getting a new one
+
+using var servicesProvider = services.BuildServiceProvider();
+
+var ediManager = servicesProvider.GetRequiredService<IManageEDI>();
+
+// Check if the EGridOS API is running
+Console.WriteLine(ediManager.CheckECGridOSIsRunning().Result);
+
+// Check that My API Key can access a persistant session.
+Console.WriteLine(ediManager.CheckECGridOSSession(MyAPIKey).Result);
+
+int downloadedFileCount = ediManager.DownloadFiles(MyAPIKey).Result;
+
+int uploadFileParcelID = ediManager.UploadloadFile(MyAPIKey).Result;
+
+Console.ReadKey();
